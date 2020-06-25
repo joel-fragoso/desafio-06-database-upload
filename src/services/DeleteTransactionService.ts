ee66b1f750581c3dcff9179c2d8ad317f500bd92
Joel Fragoso <joelfragoso85@icloud.com>
2020-06-25 13:33:03 -0300

    feature: Added transaction deletion

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const findTransactionById = await transactionsRepository.findOne(id);
+
+    if (!findTransactionById) {
+      throw new AppError('Transaction was not found');
+    }
+
+    await transactionsRepository.delete(findTransactionById.id);
   }
 }
 
